b1ac13ab8c0e0869728d54900526fb294fa81d4b
-Badrinathan-S <badrinathan3@gmail.com>
2022-05-06 20:47:01 +0530

    home page rest-api ready

--- src/main/java/com/springboot/app/controller/BookController.java
+++ src/main/java/com/springboot/app/controller/BookController.java
@@ -2,26 +2,58 @@ package com.springboot.app.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.app.entity.BookEntity;
+import com.springboot.app.service.BookService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
-	
+
+	@Autowired
+	BookService bookServ;
 
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public List<BookEntity> checkPage() {
-		
-		List<BookEntity> bookList = Arrays.asList(
-				new BookEntity("Book1", "good book1", 2020, "romance"),
-				new BookEntity("Book2", "good book2", 2021, "thriller"),
-				new BookEntity("Book3", "good book3", 2022, "horror")
-		);
-		
-		return bookList;
+
+		return bookServ.gettingAll();
 	}
+
+	@RequestMapping(value = "/send", method = RequestMethod.POST)
+	public BookEntity postBook(@RequestBody BookEntity book) {
+
+		return bookServ.posting(book);
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public BookEntity updateBook(@RequestBody BookEntity book) {
+
+		return bookServ.posting(book);
+	}
+
+	@DeleteMapping("/remove")
+	public BookEntity deleteBook(@RequestBody BookEntity book) {
+
+		return bookServ.deleting(book);
+	}
+	
+	
+	@GetMapping("/{key}")
+	public BookEntity getBook(@PathVariable("key") int key) {
+		
+		return bookServ.gettingInfo(key);
+		
+	}
+
 }
--- src/main/java/com/springboot/app/service/BookService.java
+++ src/main/java/com/springboot/app/service/BookService.java
@@ -1,8 +1,43 @@
 package com.springboot.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.springboot.app.entity.BookEntity;
+import com.springboot.app.repository.BookRepository;
 
 @Service
 public class BookService {
+	
+	@Autowired
+	BookRepository bookRepo;
+	
+	public BookEntity posting(BookEntity book) {
+		
+		BookEntity savedBook = bookRepo.save(book);
+		
+		return savedBook;
+	}
+	
+	public List<BookEntity> gettingAll(){
 
+		return (List<BookEntity>) bookRepo.findAll();
+	}
+	
+	public BookEntity gettingInfo(int key) {
+		
+		return bookRepo.findById(key).orElse(new BookEntity());
+		
+	}
+	
+	public BookEntity deleting(BookEntity book){
+		
+		bookRepo.deleteById(book.getBookId());
+		
+		return book;
+	}
+	
 }
